9c053ea48375a36c0101d807e3c719e724b4281f
H-C-21 <harshit.c21@iiits.in>
2024-10-02 18:00:32 +0530

    home

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,27 @@
-import kalidoscope from '../../assets/kalidoscope.jpeg'; // Adjust the path as necessary
-
+import kalidoscope from '../../assets/kalidoscope.jpeg';
+import tedX from '../../assets/tedX.png'; 
 
 function HomePage() {
     return (
-        <div>
-            <div className="h-[80%]">
-            <img src={kalidoscope} className="object-cover w-full h-screen object-bottom" alt="Description of image"/>
+      <div>
+        <div className="h-[80%]">
+          <div
+            className="w-full h-[85vh] bg-no-repeat bg-right-bottom flex justify-center items-center"
+            style={{
+              backgroundImage: `url(${kalidoscope})`,
+              backgroundPosition: "70% 50%",
+            }}
+          >
+
+            <div className='flex w-[70%] my-auto'>
+                <div className='flex flex-col'>
+                    <img src={tedX} className='w-48 h-48'></img>
+                    <div>IIITSriCity</div>
+                </div>
             </div>
+          </div>
         </div>
+      </div>
     );
 }
 
