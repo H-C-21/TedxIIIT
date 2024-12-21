3bd8fa1ba9d595d02347fac5df50b8ddc048f73e
AnkurRaj <142428492+ankurraj007@users.noreply.github.com>
2024-12-21 20:42:37 +0530

    Update StartingBanner.jsx

--- src/components/StartingBanner.jsx
+++ src/components/StartingBanner.jsx
@@ -1,15 +1,15 @@
-import banner from '../assets/iiit.png'
+import banner from '../assets/iiit.png';
+
 function StartingBanner() {
   return (
     <div className="h-[80%]">
       <div
         className="w-full h-[83vh] bg-cover bg-no-repeat bg-right-bottom flex justify-center items-center"
         style={{
-          backgroundImage: `url('src/assets/iiit.png')`,
+          backgroundImage: `url(${banner})`, // Use the imported variable
           backgroundPosition: "65% 50%",
         }}
       >
-
       </div>
     </div>
   );
