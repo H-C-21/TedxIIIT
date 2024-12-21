5763b7740a47e22587919a431a369e9b7379e067
TheMadKing <142428492+ankurraj007@users.noreply.github.com>
2024-12-21 20:37:48 +0530

    fixing errors

--- src/components/StartingBanner.jsx
+++ src/components/StartingBanner.jsx
@@ -1,25 +1,14 @@
 function StartingBanner() {
-  const isMobile = window.innerWidth <= 900; // Adjust the width as per your requirement
-
   return (
     <div className="h-[80%]">
       <div
         className="w-full h-[83vh] bg-cover bg-no-repeat bg-right-bottom flex justify-center items-center"
         style={{
-          backgroundImage: isMobile
-            ? `url('src/assets/mobile.png')` // Replace with the path to your mobile image
-            : `url('src/assets/iiit.png')`,
-          backgroundPosition: isMobile ? "center" : "65% 40%",
-          backgroundSize: isMobile ? "95%" : "100%",
+          backgroundImage: `url('src/assets/iiit.png')`,
+          backgroundPosition: "65% 50%",
         }}
-      ></div>
-    </div>
-  );
-}
-
-export default StartingBanner;
-
-/* <div className="flex flex-col md:flex-row w-[90%] md:w-[70%] my-auto mr-[5vw] ml-4 md:ml-7 mt-[10vh] md:mt-[22vh]">
+      >
+        {/* <div className="flex flex-col md:flex-row w-[90%] md:w-[70%] my-auto mr-[5vw] ml-4 md:ml-7 mt-[10vh] md:mt-[22vh]">
                   <div className="flex flex-col items-center md:items-start">
                       <img 
                           src={tedX} 
@@ -35,4 +24,10 @@ export default StartingBanner;
                           </p>
                       </div>
                   </div>
-              </div> */
+              </div> */}
+      </div>
+    </div>
+  );
+}
+
+export default StartingBanner;
